7cf46921957f9e4a6339183b30523c650d8748ca
rishi <rishi77177@gmail.com>
2022-07-04 02:06:05 +0530

    1.1

--- app/src/main/java/com/example/birthdaygreet/BirthdayCard.kt
+++ app/src/main/java/com/example/birthdaygreet/BirthdayCard.kt
@@ -11,7 +11,7 @@ class BirthdayCard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_card)
-        val name = intent.getStringExtra(Name)
+        val name = intent?.extras?.getString(Name)
         val textView : TextView = findViewById(R.id.textView)
         textView.text = "Happy Birthday\n $name!"
     }
